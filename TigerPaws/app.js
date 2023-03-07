3f9dce03eb97aba17b97f78a812177bd90d72a7c
Audrey Deering <audreydeering@audreys-air.trinity.local>
2023-03-07 17:23:56 -0600

    Successfully rerouted login page to different page.

--- TigerPaws/app.js
+++ TigerPaws/app.js
@@ -13,6 +13,10 @@ app.route("/login").get((req, res) => {
     res.render('pages/login.ejs');
 });
 
+app.route("/successfulLogin").get((req, res) => {
+    res.render('pages/successfulLogin.ejs');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
