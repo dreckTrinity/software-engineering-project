d7d929104e6c313c7b426c8d35469aad6a807239
dreckTrinity <99363112+dreckTrinity@users.noreply.github.com>
2023-03-13 01:46:59 -0500

    New More 𝒔𝒕𝒓𝒆𝒂𝒎𝒍𝒊𝒏𝒆𝒅 home page
    Rebuilt to be a bit more modular and not reliant on tweaking random values. Still looks pretty similar to the mockups.

--- TigerPaws/app.js
+++ TigerPaws/app.js
@@ -13,6 +13,11 @@ app.route("/login").get((req, res) => {
     res.render('pages/login.ejs');
 });
 
+app.route("/professor-page").get((req, res) => {
+    res.render('pages/professor-page.ejs');
+});
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
