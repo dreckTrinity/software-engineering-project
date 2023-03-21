14169eca4e1c0d1b34739e0e6ca0dbbfd2638829
Agus <flon@aguss-air.trinity.local>
2023-03-21 18:01:38 -0500

    Added working buttons to administrator remake

--- TigerPaws/app.js
+++ TigerPaws/app.js
@@ -18,6 +18,15 @@ app.route("/login").get((req, res) => {
 app.route("/successfulLogin").get((req, res) => {
     res.render('pages/successfulLogin.ejs');
 });
+app.route("/administrator-remake").get((req,res) => {
+    res.render('pages/administrator-remake');
+});
+app.route("/manage-courses-student").get((req,res) => {
+    res.render('pages/manage-courses-student');
+});
+app.route("/course-list-page").get((req,res) => {
+    res.render('pages/course-list-page');
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
