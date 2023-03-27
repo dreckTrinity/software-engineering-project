ab6538c1651ea96ee1898e65c65c63b6f312293e
dreckTrinity <99363112+dreckTrinity@users.noreply.github.com>
2023-03-27 10:57:31 -0500

    Added Search and Help page
    Added Search Page, Help page, and edited login pages. Search and help need more work and login is hit or miss right now

--- TigerPaws/app.js
+++ TigerPaws/app.js
@@ -3,16 +3,25 @@ const bodyParser = require('body-parser');
 const app = express();
 var path = require('path')
 
+//Loads Resources file to let the images load, check login remake for example of how to load an image from Resources
+app.use(express.static('public')); 
+app.use('/Resources', express.static('Resources'));
+
+//Loads CSS file
+app.use(express.static('public'));
+app.use('/views/CSS', express.static('CSS'));
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
 app.route("/").get((req, res) => {
-    res.render('pages/login.ejs');
+    
+    res.render('pages/login-remake.ejs');
 });
 
 app.route("/login").get((req, res) => {
-    res.render('pages/login.ejs');
+    res.render('pages/login-remake.ejs');
 });
 
 app.route("/successfulLogin").get((req, res) => {
