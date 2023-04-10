83f7d7514b8af7453d01e7806881e55511087282
dreckTrinity <99363112+dreckTrinity@users.noreply.github.com>
2023-04-10 12:43:55 -0500

    Search slightly polished + started /addCourse work

--- TigerPaws/api/routes/courses.js
+++ TigerPaws/api/routes/courses.js
@@ -22,12 +22,13 @@ app.get("/courses", async (req, res) => {
   })
 
   app.post("/courseSearch", async(req, res) => {
+    //Gets the input from the search box on the search page, currently just takes course number
     const course = req.body.course
-    console.log("request made")
-    console.log(course)
     
+    //Checks the database based on course and puts all matches into an array
     const courseMatches = await courseModel.find({courseNum: course}).exec();
-    console.log(courseMatches)
+
+    //Checks if the array is empty, if it is then return to search page, if not then render list page with the info from course matches
     if(courseMatches.length == 0){
       console.log("No course found. Try harder please")
       res.render(path.join(__dirname, '/../../views/pages/course-search-page.ejs'))
@@ -35,5 +36,23 @@ app.get("/courses", async (req, res) => {
       res.render(path.join(__dirname, '/../../views/pages/course-list-page.ejs') , {courses:courseMatches})
     }
     //print courses into log
+  })
+
+  app.post("/addCourse", async(req, res) => {
+
+    //This big block gets all the data from the form and makes it into a usable form
+    const courseName = req.body.courseName
+    const courseNum = req.body.courseNum
+    const courseDept = req.body.dept
+    const courseInstrutor = req.body.courseInstrutor
+    const courseHours = req.body.courseHours
+    const coursePathways = req.body.coursePathways
+    const coursePrereqs = req.body.coursePrereqs
+    const courseBulding = req.body.courseBulding
+    const courseRoomNum = req.body.courseRoomNum
+    const courseMeetingDays = req.body.courseMeetingDays
+    const courseDescription = req.body.courseDescription
+    //COURSE SECTION NEEDS TO BE ADDED, IT IS CURRENTLY ALWAYS ADDED AS 1
+    //ALSO LIKE, ACTUALLY ADDING THE COURSE NEEDS TO BE ADDED
   })
 module.exports = app
